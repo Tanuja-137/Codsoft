39ab419dc33fe12c6e4ca4277f7e36d99cad38c7
Tanuja-137 <nelapatitanuja7@gmail.com>
2024-07-31 11:36:49 +0530

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,13 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function deleteDisplay() {
+    display.value = "";
+
+}
+function Calculate() {
+    display.value = eval(display.value);
+}
